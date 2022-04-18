752c5e7a299c61d8085614cd26ed59580fd0ad0d
ko50 <kotyana1331@gmail.com>
2022-04-18 16:27:48 +0900

    fix: use TransitionList

--- src/api/mock/transition.ts
+++ src/api/mock/transition.ts
@@ -1,50 +1,56 @@
-import { Transition } from "models/transition";
+import { Prefecture } from "models/prefecture";
+import { TransitionList } from "models/transition";
 
-export async function getMockedPopulationTransition(): Promise<Transition[]> {
-    const prefList: Transition[] = [
-        {
-            boundaryYear: 2020,
-            populations: [
-                {
-                    year: 2018,
-                    value: 2000,
-                },
-                {
-                    year: 2019,
-                    value: 4000,
-                },
-                {
-                    year: 2020,
-                    value: 6000,
-                },
-                {
-                    year: 2021,
-                    value: 8000,
-                },
-            ],
-        },
-        {
-            boundaryYear: 2020,
-            populations: [
-                {
-                    year: 2018,
-                    value: 1000,
-                },
-                {
-                    year: 2019,
-                    value: 2000,
-                },
-                {
-                    year: 2020,
-                    value: 3000,
-                },
-                {
-                    year: 2021,
-                    value: 4000,
-                },
-            ],
-        },
-    ];
+export async function getMockedPopulationTransition(
+    prefList: Prefecture[]
+): Promise<TransitionList> {
+    const transition: TransitionList = {
+        boundaryYear: 2020,
+        transitions: [
+            {
+                pref: "茨城県",
+                populations: [
+                    {
+                        year: 2018,
+                        value: 2000,
+                    },
+                    {
+                        year: 2019,
+                        value: 4000,
+                    },
+                    {
+                        year: 2020,
+                        value: 6000,
+                    },
+                    {
+                        year: 2021,
+                        value: 8000,
+                    },
+                ],
+            },
+            {
+                pref: "東京都",
+                populations: [
+                    {
+                        year: 2018,
+                        value: 1000,
+                    },
+                    {
+                        year: 2019,
+                        value: 2000,
+                    },
+                    {
+                        year: 2020,
+                        value: 3000,
+                    },
+                    {
+                        year: 2021,
+                        value: 4000,
+                    },
+                ],
+            },
+        ],
+    };
 
-    return prefList;
+    return transition;
 }
