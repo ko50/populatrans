b8a5e4117f710094da54bf7e2edefa63c63e5a69
ko50 <kotyana1331@gmail.com>
2022-04-18 23:27:32 +0900

    feat: add style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { PrefForm } from "components/checkbox/PrefForm";
 import { PopulationTransitionGraph } from "components/graph/PopulationTransition";
+import { Header } from "components/header/Common";
 import { H1 } from "components/heading/H1";
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "styles/pages/index.module.scss";
 
 const Index: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>POPULA Trans</title>
         <meta
@@ -18,15 +19,18 @@ const Index: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div>
-          <H1 text="人口の推移" />
-          <PopulationTransitionGraph
-            transition={{ boundaryYear: 2020, transitions: [] }}
-          />
-        </div>
-        <div>
-          <H1 text="都道府県" />
-          <PrefForm prefList={[]} onSelect={() => {}} />
+        <Header />
+        <div className={styles.wrapper}>
+          <div className={styles.graph}>
+            <H1 text="人口の推移" />
+            <PopulationTransitionGraph
+              transition={{ boundaryYear: 2020, transitions: [] }}
+            />
+          </div>
+          <div className={styles.prefForm}>
+            <H1 text="都道府県" />
+            <PrefForm prefList={[]} onSelect={() => {}} />
+          </div>
         </div>
       </main>
     </div>
