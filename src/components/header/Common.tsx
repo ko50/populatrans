68875b5f14f87a6fdbe115adbf8b2d0531a96518
ko50 <kotyana1331@gmail.com>
2022-04-18 23:38:36 +0900

    fix: rewrite header sentence

--- src/components/header/Common.tsx
+++ src/components/header/Common.tsx
@@ -1,5 +1,5 @@
 import styles from "styles/components/header.module.scss";
 
 export const Header: React.FC = () => {
-  return <div className={styles.header}>Header</div>;
+  return <div className={styles.header}>POPULATrans</div>;
 };
