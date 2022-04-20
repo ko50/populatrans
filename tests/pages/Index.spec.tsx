045bf90ec8cdd0778c9a347870ceaf971d549e4e
ko <60608740+ko50@users.noreply.github.com>
2022-04-20 11:36:31 +0900

    feat: api instance declear globally (#56)
    * feat: declear api globally
    
    * fix: fix index page
    
    * fix: forgot fix test

--- tests/pages/Index.spec.tsx
+++ tests/pages/Index.spec.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Index from "pages/index";
-import { getMockedPrefectureList } from "api/mock/prefList";
-import { getMockedPopulationTransition } from "api/mock/transition";
 
 describe("common home page", () => {
   it("should render correctly", () => {
-    const tree = renderer
-      .create(
-        <Index
-          prefList={[]}
-          api={{
-            prefListAPI: getMockedPrefectureList,
-            populationTransitionAPI: getMockedPopulationTransition,
-          }}
-        />
-      )
-      .toJSON();
+    const tree = renderer.create(<Index prefList={[]} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
