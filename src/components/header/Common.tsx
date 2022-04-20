e9ca4bd5ed93e998447e70a89a1a8624af033140
ko <60608740+ko50@users.noreply.github.com>
2022-04-20 19:55:50 +0900

    feat: responsive support (#58)
    * feat: make index page responsive
    
    * feat: make form responsive
    
    * feat: make haeding responsive
    
    * fix: change padding style at index page
    
    * fix: shrink form text when sm
    
    * feat: make header responsive
    
    * feat: make graph responsive?
    
    * fix: change styles dir struecture

--- src/components/header/Common.tsx
+++ src/components/header/Common.tsx
@@ -1,4 +1,4 @@
-import styles from "styles/components/header.module.scss";
+import styles from "styles/components/header/common.module.scss";
 
 export const Header: React.FC = () => {
   return <div className={styles.header}>POPULATrans</div>;
