a3143943f5bc2457123211b65d3d9ea47a863a98
ko50 <kotyana1331@gmail.com>
2022-04-21 12:36:34 +0900

    feat: add resas client

---
+++ src/api/resas/config/client.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+
+export function initializeResasClient(): AxiosInstance {
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+    if (apiKey === undefined)
+        throw new Error(
+            "Please provide API key by setting 'API_KEY' in environment var!"
+        );
+
+    const config: AxiosRequestConfig = {
+        headers: {
+            "X-API-KEY": apiKey!,
+        },
+    };
+
+    return axios.create(config);
+}
+
+export const resasClient = initializeResasClient();
