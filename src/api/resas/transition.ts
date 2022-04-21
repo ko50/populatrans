9173b0609edafdaee806a13ba8691e4a14661ce0
ko50 <kotyana1331@gmail.com>
2022-04-21 14:27:13 +0900

    fix: take client

--- src/api/resas/transition.ts
+++ src/api/resas/transition.ts
@@ -1,6 +1,6 @@
-import { resasClient } from "api/resas/config/client";
 import { RESAS_BASE_PATH } from "api/resas/config/constants";
 import { Response } from "api/resas/config/types";
+import { AxiosInstance } from "axios";
 import { Prefecture } from "models/prefecture";
 import { Transition, TransitionList } from "models/transition";
 
@@ -19,6 +19,7 @@ type ResponseData = {
 };
 
 export async function getResasPopulationTransition(
+    client: AxiosInstance,
     selected: Prefecture[]
 ): Promise<TransitionList> {
     const data: Transition[] = [];
@@ -26,12 +27,9 @@ export async function getResasPopulationTransition(
 
     for (const p of selected) {
         await (async () => {
-            const url =
-                RESAS_BASE_PATH +
-                "/population/composition/perYear?prefCode=" +
-                p.code +
-                "&cityCode=-";
-            const _res = await resasClient.get<Response<ResponseData>>(url);
+            const url = RESAS_BASE_PATH + "/population/composition/perYear";
+            const query = "?prefCode=" + p.code + "&cityCode=-"; // 全ての市区町村から取得
+            const _res = await client.get<Response<ResponseData>>(url + query);
 
             const result = _res.data.result;
             const datum: Transition = {
